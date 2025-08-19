3d73e163ebf5e22ae730c33ebcdd77c893eb6655
SungPhilJoon <soung2849@gmail.com>
2025-08-19 15:10:42 +0900

    WBP_Crosshair 추가

---
+++ TPSTeamProject/Source/TPSTeamProject/Private/UI/CrosshairComponent.cpp
@@ -0,0 +1,85 @@
+#include "UI/CrosshairComponent.h"
+#include "Character/ShooterCharacter.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UCrosshairComponent::UCrosshairComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+    MinAimSize = 0.f;
+    MaxAimSize = 500.f;
+}
+
+
+void UCrosshairComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (CrosshairWidget)
+	{
+		CrosshairWidget->AddToViewport();
+	}
+}
+
+
+void UCrosshairComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UpdateCrosshair(DeltaTime);
+}
+
+
+void UCrosshairComponent::UpdateCrosshair(float DeltaTime)
+{
+    if (!CrosshairWidget)
+    {
+        return;
+    }
+
+    AActor* Owner = GetOwner();
+    if (Owner)
+    {
+        FVector Velocity = Owner->GetVelocity();
+        float Speed = Velocity.Size();
+
+        AShooterCharacter* Character = Cast<AShooterCharacter>(Owner);
+        bool IsJumping = Character->GetCharacterMovement()->IsFalling();
+        bool IsShooting = Character->bIsShoot;
+
+        if (Speed == 0 && AimSize == 0 && !IsShooting)
+        {
+            return;
+        }
+
+        if (IsJumping) 
+        {
+            AimSize += 2000.f * DeltaTime;
+        }
+        else 
+        {
+            if (Speed > 100)
+            {
+                AimSize += Speed * DeltaTime;
+            }
+            else if (Speed > 0)
+            {
+                AimSize -= FMath::Clamp(Speed * DeltaTime * 50, 0.0f, AimSize);
+            }
+            else
+            {
+                AimSize -= FMath::Clamp(2000.f * DeltaTime, 0.0f, AimSize);
+            }
+        }
+
+        if (IsShooting)
+        {
+            AimSize += 2000.f * DeltaTime;
+        }
+        
+        AimSize = FMath::Clamp(AimSize, MinAimSize, MaxAimSize);
+        CrosshairWidget->UpdateCrosshairSize(AimSize);
+    }
+}
+
---
+++ TPSTeamProject/Source/TPSTeamProject/Public/UI/CrosshairWidget.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "CrosshairWidget.generated.h"
+
+UCLASS()
+class TPSTEAMPROJECT_API UCrosshairWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> LineTop;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> LineBottom;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> LineLeft;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> LineRight;
+
+	UFUNCTION(BlueprintCallable, Category = "Crosshair")
+	void UpdateCrosshairSize(const float Speed);
+	
+};
